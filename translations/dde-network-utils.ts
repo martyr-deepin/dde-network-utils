86d6cb206cfc1415af694ffef0126b6d9d9853ea
listenerri <listenerri@gmail.com>
2018-06-15 10:32:18 +0800

    add: translations
    Change-Id: Ia23e81a4a014efdb924d297d50f8a1163d5565e2

---
+++ translations/dde-network-utils.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>dde::network::NetworkDevice</name>
+    <message>
+        <location filename="../networkdevice.cpp" line="81"/>
+        <source>Disconnected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../networkdevice.cpp" line="83"/>
+        <source>Connecting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../networkdevice.cpp" line="84"/>
+        <source>Authenticating</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../networkdevice.cpp" line="86"/>
+        <source>Obtaining Address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../networkdevice.cpp" line="87"/>
+        <source>Connected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../networkdevice.cpp" line="89"/>
+        <source>Failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
