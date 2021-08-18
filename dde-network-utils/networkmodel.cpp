561b174bbf28bcf655588c9781e42a4feb5a3d37
black_desk <clx814727823@gmail.com>
2021-08-18 11:27:21 +0800

    perf: 优化代码逻辑
    在 I52342ceb0b3b3a8be30470d92af7e1ddd29b510d 中修改代码时没有考虑清楚代
    码逻辑流程, 现经 Iedc67f0a634002f318e87383285231312452d80a 提醒发现实际
    上设备只会匹配一次. 于是增加一个 break.
    
    Log:
    Task: https://pms.uniontech.com/zentao/task-view-79863.html
    Change-Id: Ia874042223b51dace6236ece2acca1d2a0c28733

--- dde-network-utils/networkmodel.cpp
+++ dde-network-utils/networkmodel.cpp
@@ -672,6 +672,7 @@ void NetworkModel::onWirelessAccessPointsChanged(const QString &WirelessList)
             //当类型不为无线网,path不为当前需要的device则进入下一个循环
             if (dev->type() != NetworkDevice::Wireless || dev->path() != Device) continue;
             dynamic_cast<WirelessDevice *>(dev)->setAPList(WirelessData.value(Device));
+            break;
         }
     }
     return;
